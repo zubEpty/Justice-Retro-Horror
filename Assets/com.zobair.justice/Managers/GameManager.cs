f1cd0990c5510790a6ac1911ac7d6fff87f29870
Zobair hasan <103631576+QZenQ@users.noreply.github.com>
2025-05-03 14:02:42 +0600

    Added manager and updated collider in killer

---
+++ Assets/com.zobair.justice/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    
+    void Awake()
+    {
+        // Singleton pattern to ensure only one GameManager exists
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Persist across scenes
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public LightManager LightManager_;
+
+    void Update()
+    {
+        
+    }
+
+ 
+}
